a1a4fca893e714297bddbea9a97082141f3f5c70
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-09-27 23:09:18 +0500

    Backup

--- server.js
+++ server.js
@@ -112,8 +112,10 @@ app.use((err, req, res, next) => {
   });
 });
 
-
-module.exports = app;
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
+});
 
 
 // console.log('Cloudinary config:', process.env.CLOUDINARY_CLOUD_NAME)
