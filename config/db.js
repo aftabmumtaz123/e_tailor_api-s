db2cfd3ec9da40a5c2d5b55b60fc6298d9610583
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-09-27 21:04:18 +0500

    sdas

--- config/db.js
+++ config/db.js
@@ -3,8 +3,6 @@ require("dotenv").config()
 mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 20000, // Increased to 20 seconds
-  socketTimeoutMS: 45000 // Optional: Increased socket timeout
 }).then(()=>{
     console.log("Connection Successful");
 }).catch((e)=>{
