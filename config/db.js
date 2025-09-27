9ca762e7912af0a3fb00f7f71b47d2957ab98920
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-09-27 20:50:13 +0500

    changed ip

--- config/db.js
+++ config/db.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config()
-mongoose.connect(process.env.MONGODB_URL).then(()=>{
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 20000, // Increased to 20 seconds
+  socketTimeoutMS: 45000 // Optional: Increased socket timeout
+}).then(()=>{
     console.log("Connection Successful");
 }).catch((e)=>{
     console.log("There is some error in connection",e);
