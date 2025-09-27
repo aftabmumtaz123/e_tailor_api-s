176a1c7da75a7c7631a89d3c3f1af5b2429e25c0
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-09-27 21:07:51 +0500

    qwerty

--- config/db.js
+++ config/db.js
@@ -1,10 +1,18 @@
-const mongoose = require("mongoose");
-require("dotenv").config()
-mongoose.connect(process.env.MONGODB_URL,{
-    useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(()=>{
-    console.log("Connection Successful");
-}).catch((e)=>{
-    console.log("There is some error in connection",e);
-});
+import mongoose from "mongoose";
+
+let isConnected;
+
+export async function connectDB() {
+  if (isConnected) return;
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = db.connections[0].readyState;
+    console.log("MongoDB connected:", isConnected);
+  } catch (error) {
+    console.error("DB connection failed:", error);
+  }
+}
