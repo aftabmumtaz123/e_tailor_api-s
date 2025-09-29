b510cefb07061b970d6000cb6e532d1bfb0e6a7d
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-09-29 11:28:14 +0500

    setting up mongodb

--- config/db.js
+++ config/db.js
@@ -1,22 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
 
-let isConnected = false;
-
-async function connectDB() {
-  if (isConnected) return;
-
-  try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    isConnected = conn.connections[0].readyState;
-    console.log("✅ MongoDB connected:", conn.connection.host);
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err.message);
-    throw err;
-  }
-}
-
-module.exports = connectDB;
+    console.log("Connection Successful");
+}).catch((e) => {
+    console.log("There is some error in connection", e);
+});
