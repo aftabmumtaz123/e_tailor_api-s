48f135b63725107cabe820d0af4466b94a8bd455
Aftab Mumtaz <110008432+aftabmumtaz123@users.noreply.github.com>
2025-09-29 13:49:03 +0500

    hsdhjjd

--- router/announcementRouter.js
+++ router/announcementRouter.js
@@ -9,8 +9,8 @@ const {getAnnouncement, createAnnouncement, deleteAnnouncement} = require("../co
 
 
 
-router.get("/list", getAnnouncement);
-router.post('/create', upload.single('announcementImage'), createAnnouncement);
-router.delete("/delete/:id", deleteAnnouncement);
+router.get("/list", authMiddleware ,getAnnouncement);
+router.post('/create', authMiddleware ,upload.single('announcementImage'), createAnnouncement);
+router.delete("/delete/:id", authMiddleware ,deleteAnnouncement);
 
 module.exports = router;
